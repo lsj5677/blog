d39906c86303c592b0aea1fa92cf04f77b25a16f
lsj5677 <lsj5677@naver.com>
2024-06-08 11:29:07 -0400

    feat: add slug

--- src/app/works/[slug]/page.tsx
+++ src/app/works/[slug]/page.tsx
@@ -1,3 +1,27 @@
-export default function WorkSlugPage() {
-  return <div></div>;
+import { getPost } from "@/service/posts";
+import { redirect } from "next/navigation";
+
+type Props = {
+  params: { slug: string };
+};
+
+export function generateMetadata({ params }: Props) {
+  return {
+    title: `Works | ${params.slug}`,
+  };
+}
+
+export default async function WorkSlugPage({ params: { slug } }: Props) {
+  const post = await getPost(slug);
+  console.log(`post`, post);
+  if (!post) {
+    redirect("/works");
+  }
+
+  return (
+    <div className="px-5">
+      <h2>{post?.title}</h2>
+      <p>{post?.content}</p>
+    </div>
+  );
 }
--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -6,8 +6,10 @@ export default async function WorksPage() {
   const posts = await getAllPosts();
   const categories = [...new Set(posts.map((post) => post.category))];
   return (
-    <div className={`px-5 ${sans_serif.className}`}>
-      <FilterableWorks posts={posts} categories={categories} />
+    <div className={`px-5 py-10 ${sans_serif.className}`}>
+      <div className="sub-wrap">
+        <FilterableWorks posts={posts} categories={categories} />
+      </div>
     </div>
   );
 }
