98000b7971d0365b8677db8fa7d31c336b42ff3e
lsj5677 <lsj5677@naver.com>
2024-06-10 17:26:14 -0400

    feat: add markdownviewr w/react-syntax-highlight, remarkGfm etc.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        // 사용하고자 하는 도메인 URL
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+};
 export default nextConfig;
--- src/app/works/[slug]/page.tsx
+++ src/app/works/[slug]/page.tsx
@@ -1,5 +1,8 @@
+import { sans_serif } from "@/app/layout";
+import MarkdownViewer from "@/components/MarkdownViewer";
 import { getPost } from "@/service/posts";
-import { redirect } from "next/navigation";
+import Image from "next/image";
+import { CiCalendarDate } from "react-icons/ci";
 
 type Props = {
   params: { slug: string };
@@ -12,16 +15,32 @@ export function generateMetadata({ params }: Props) {
 }
 
 export default async function WorkSlugPage({ params: { slug } }: Props) {
-  const post = await getPost(slug);
-  console.log(`post`, post);
-  if (!post) {
-    redirect("/works");
-  }
+  const { title, description, date, path, content } = await getPost(slug);
 
   return (
-    <div className="px-5">
-      <h2>{post?.title}</h2>
-      <p>{post?.content}</p>
-    </div>
+    <article className={`my-10 px-5 ${sans_serif.className}`}>
+      <div className="sub-wrap">
+        <Image
+          src={`/images/posts/${path}.png`}
+          alt={title}
+          width={800}
+          height={200}
+          className="mx-auto my-4 h-1/6 max-h-[400px] w-full"
+        />
+        <div className="mb-8 border-b border-third pb-4 md:pb-8">
+          <div className="flex items-start justify-between">
+            <div>
+              <h2 className="text-lg font-semibold md:text-2xl">{title}</h2>
+            </div>
+            <div className="flex shrink-0 items-center gap-1 text-xs">
+              <CiCalendarDate className="text-xl" />
+              <time>{date.toString()}</time>
+            </div>
+          </div>
+          <p className="md:text-md text-sm">{description}</p>
+        </div>
+        <MarkdownViewer content={content} />
+      </div>
+    </article>
   );
 }
