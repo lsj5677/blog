7e602b5a60b5e733315be93070b754ced10afdcf
lsj5677 <lsj5677@naver.com>
2024-06-10 18:33:46 -0400

    feat: add next, prev in works

--- src/app/works/[slug]/page.tsx
+++ src/app/works/[slug]/page.tsx
@@ -1,4 +1,6 @@
 import { sans_serif } from "@/app/layout";
+import AdjacentSection from "@/components/AdjacentSection";
+import Backbutton from "@/components/Backbutton";
 import WorkContent from "@/components/WorkContent";
 import { getPost } from "@/service/posts";
 
@@ -14,11 +16,18 @@ export function generateMetadata({ params }: Props) {
 
 export default async function WorkSlugPage({ params: { slug } }: Props) {
   const post = await getPost(slug);
-
+  const { next, prev } = post;
   return (
     <article className={`my-10 px-5 ${sans_serif.className}`}>
       <div className="sub-wrap">
         <WorkContent post={post} />
+        <section>
+          <div className="my-10 flex justify-between border-b border-t border-third pb-10 pt-10">
+            {prev && <AdjacentSection type="prev" post={prev} />}
+            {next && <AdjacentSection type="next" post={next} />}
+          </div>
+          <Backbutton />
+        </section>
       </div>
     </article>
   );
