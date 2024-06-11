1b248caa6c711c8f7e81259c06c1da53ff2324b8
lsj5677 <lsj5677@naver.com>
2024-06-10 23:04:41 -0400

    style: update footer style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="w-full max-w-screen-xl border-t bg-third py-3 text-right text-xs">
+    <footer className="w-full border-t bg-third py-3 text-right text-xs">
       <div className="sub-wrap">
         <p>{"This is SUJIN's | All right Reserved"}</p>
       </div>
